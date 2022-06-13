d7c2aebdf56a5e5e90bf7ca860111de00db87209
ahsun rahim <ahsunrahim@gmail.com>
2022-06-14 04:47:04 +0600

    68 update

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,11 +1,12 @@
 import React, { useRef } from 'react';
+import axios from 'axios';
 import { Button, Form } from 'react-bootstrap';
 import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
-import { ToastContainer, toast } from 'react-toastify';
+import {toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PageTitle from '../../Shared/PageTitle/PageTitle';
 
@@ -31,19 +32,22 @@ const Login = () => {
     }
 
     if (user) {
-        navigate(from, { replace: true });
+        //navigate(from, { replace: true });
     }
 
     if (error) {
         errorElement = <p className='text-danger'>Error: {error?.message}</p>
     }
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        const {data} = await axios.post('http://localhost:5000/login', {email});
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate(from, { replace: true });
     }
 
     const navigateRegister = event => {
@@ -77,10 +81,10 @@ const Login = () => {
                 </Button>
             </Form>
             {errorElement}
-            <p>New to Genius Car? <Link to="/register" className='text-primary pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link> </p>
+            <p>New to this Car service? <Link to="/register" className='text-primary pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link> </p>
             <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>
             <SocialLogin></SocialLogin>
-            <ToastContainer />
+            
         </div>
     );
 };
---
+++ src/Pages/Orders/Orders.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { signOut } from 'firebase/auth';
+import { Navigate } from 'react-router';
+import axiosPrivate from '../../Api/AxiosPrivate';
+
+const Orders = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+    useEffect( () =>  {
+
+        const getOrders = async() => {
+            const email = user.email;
+            const url = `http://localhost:5000/orders?email=${email}`;
+            try{
+                const {data} = await axiosPrivate.get(url);
+                setOrders(data);
+            }
+            catch(error){
+                console.log(error.message);
+                if(error.response.status === 401 || error.response.status === 403){
+                    signOut(auth);
+                    Navigate('/login')
+                }
+            }
+        }
+            getOrders();
+    }, [user])
+    return (
+        <div>
+            <h2>
+                Your Orders: {orders.length}
+            </h2>
+        </div>
+    );
+};
+
+export default Orders; 
